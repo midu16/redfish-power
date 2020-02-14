a4544a46a5fc04e6f43b13e7321aa404ab08a69e
Mihai I <idumihai16@gmail.com>
2020-02-14 08:02:01 +0100

    Update power.py

--- power.py
+++ power.py
@@ -1,55 +1,23 @@
-#
-# Idu Mihai - 2020
-#
+
+import requests, json, sys, re, time, warnings
+
+warnings.filterwarnings("ignore")
+
+try:
+    idrac_ip = sys.argv[1]
+    idrac_username = sys.argv[2]
+    idrac_password = sys.argv[3]
+
+except:
+    print("- FAIL: You must pass in script name along with iDRAC IP / iDRAC username / iDRAC password. Example: \"script_name.py 192.168.0.120 root calvin\"")
+    sys.exit()
 
 
-import sys
-import redfish
-import base64
 
-# When running remotely connect using the address, account name, 
-# and password to send https requests
-login_host = "https://192.168.1.100"
-login_account = "admin"
-login_password = "password"
+response = requests.get('https://%s/redfish/v1/Systems/System.Embedded.1/' % idrac_ip,verify=False,auth=(idrac_username, idrac_password))
+data = response.json()
+print("\n- WARNING, Current server power state is: %s\n" % data[u'PowerState'])
 
-
-stringTomatch = 'username_idrac:'
-matchedLine = ''
-with open('login_account','r') as file:
-        for line in file:
-            if stringToMatch in line:
-                matchedLine = line
-                break
-
-login_account_idrac=matchedLine
-#login_account_idrac=base64.b64encode("matchedLine".encode("utf-8"))
-
-stringTomatch = 'password_idrac:'
-matchedLine = ''
-with open('login_account','r') as file:
-        for line in file:
-            if stringToMatch in line:
-                matchedLine = line
-                break
-
-login_password_idrac=matchedLine
-#login_account_idrac=base64.b64encode("matchedLine".encode("utf-8"))
-
-## Create a REDFISH object
-REDFISH_OBJ = redfish.redfish_client(base_url=login_host, username=base64.b64decode("login_account_idrac".decode("utf-8")), \
-                          password=base64.b64decode("login_password_idrac".decode("utf-8")), default_prefix='/redfish/v1')
-
-# Login into the server and create a session
-REDFISH_OBJ.login(auth="session")
-
-# Do a GET on a given path
-response_idrac = REDFISH_OBJ.get("/redfish/v1/Chassis/System.Embedded.1/Power", None)
-
-response_ilo = REDFISH_OBJ.get("/redfish/v1/Chassis/1/Power", None)
-
-# Print out the response
-sys.stdout.write("%s\n" % response_idrac)
-
-# Logout of the current session
-REDFISH_OBJ.logout()
+print("- Supported values for server power control are:\n")
+for i in data[u'Actions'][u'#ComputerSystem.Reset'][u'ResetType@Redfish.AllowableValues']:
+    print(i)
